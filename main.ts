66b290b255d29d1eae3e3625ebc035d8ecadfd4b
Danilo <bonodanilo@outlook.it>
2025-04-11 11:18:33 +0200

    first commit

---
+++ main.ts
@@ -0,0 +1,15 @@
+console.log("Hello, World!");
+console.log("This is a test.");
+
+interface MyInterface {
+myNumber: number;
+myString: string;
+}      
+
+let myInterface: MyInterface = {
+myNumber: 1,    
+myString: "Hello",
+};
+
+console.log('myNumber = ', myInterface.myNumber);
+console.log('myString = ', myInterface.myString);
